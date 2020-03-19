bd6b129af6653d2b51e6e8aedbc77c4e831b06aa
Jihun Kim <starlettkim@gmail.com>
2020-03-19 23:15:16 +0900

    [#1] Impl write_association_rules

--- assignment1/src/main.cpp
+++ assignment1/src/main.cpp
@@ -4,11 +4,21 @@
 #include <string>
 #include <sstream>
 #include <iostream>
+#include <map>
+#include <set>
+#include <cmath>
 
 using namespace std;
 
-using item_id_t     = int;
+using item_id_t = int;
 using txn_t = vector<item_id_t>;
+
+typedef struct {
+  set<item_id_t> item_set;
+  set<item_id_t> associative_item_set;
+  float support;
+  float confidence;
+} AssociationRule;
 
 vector<txn_t> read_transactions(ifstream &ifs) {
     vector<txn_t> ret;
@@ -25,30 +35,56 @@ vector<txn_t> read_transactions(ifstream &ifs) {
     return ret;
 }
 
+vector<AssociationRule> find_association_rules(const vector<txn_t>& txns, int min_support) {
+    vector<AssociationRule> ret;
+
+    // TODO
+
+    return ret;
+}
+
+void write_association_rules(ofstream &ofs, const vector<AssociationRule>& rules) {
+    for (auto rule : rules) {
+        ofs << "{" << *rule.item_set.begin();
+        for (auto it = ++rule.item_set.begin(); it != rule.item_set.end(); ++it) {
+            ofs << "," << *it;
+        }
+        ofs << "}\t";
+        ofs << "{" << *rule.associative_item_set.begin();
+        for (auto it = ++rule.item_set.begin(); it != rule.associative_item_set.end(); ++it) {
+            ofs << "," << *it;
+        }
+        ofs << "}\t";
+        ofs << setprecision(2) << rule.support << "\t";
+        ofs << setprecision(2) << rule.confidence << "\n";
+    }
+}
+
 int main(int argc, const char *argv[]) {
     if (argc < 4) {
         cerr << "usage: apriori [0-100] input_file output_file" << endl;
         return 1;
     }
 
-    long min_support = strtol(argv[1], nullptr, 10);
+    const int min_support = (int) strtol(argv[1], nullptr, 0);
     ifstream ifs(argv[2]);
-    ifstream ofs(argv[3]);
-    if (!ifs.good()) {
-        cerr << "cannot open file: " << argv[2];
+    ofstream ofs(argv[3]);
+    if (min_support < 0 || min_support > 100) {
+        cerr << "minimum support must be [0-100].";
+        return 1;
+    } else if (!ifs.good()) {
+        cerr << "cannot open input file: " << argv[2];
         return 1;
     } else if (!ofs.good()) {
-        cerr << "cannot open file: " << argv[3];
+        cerr << "cannot open output file: " << argv[3];
         return 1;
     }
 
-    auto vec_txn = read_transactions(ifs);
+    auto txns = read_transactions(ifs);
+    auto asc_rules = find_association_rules(txns, min_support);
+    write_association_rules(ofs, asc_rules);
 
-    for (auto i : vec_txn) {
-        for (auto j : i)
-            cout << j << " ";
-        cout << endl;
-    }
-
+    ifs.close();
+    ofs.close();
     return 0;
 }

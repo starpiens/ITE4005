1f9b3751bd5d880c990d29253c9e639d4da53e30
Jihun Kim <starlettkim@gmail.com>
2020-06-14 22:40:24 +0900

    [4] Improve model n write results to file

--- assignment4/main.py
+++ assignment4/main.py
@@ -4,7 +4,7 @@ from os import path
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import models, layers, metrics, losses, optimizers, callbacks, activations
+from tensorflow.keras import models, layers, metrics, losses, optimizers, callbacks
 import tensorflow.keras.backend as K
 
 
@@ -30,6 +30,19 @@ def read_records(path: str) \
     return records
 
 
+def write_records(path: str, records: Dict[str, np.ndarray]) \
+        -> None:
+    f = open(path, 'w')
+    attr_names = ['user_id', 'item_id', 'rating', 'timestamp']
+
+    for idx in range(len(records['user_id'])):
+        for name in attr_names:
+            f.write(str(records[name][idx][0]) + '\t')
+        f.write('\n')
+
+    f.close()
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("train_path", help="path of training data file", type=str)
@@ -37,41 +50,44 @@ def parse_args():
     return parser.parse_args()
 
 
+def cf_model(n_users, n_movies):
+    user_input = layers.Input(shape=(1,))
+    user_x = layers.Embedding(n_users, 20, input_length=1, name='user_embed')(user_input)
+    item_input = layers.Input(shape=(1,))
+    item_x = layers.Embedding(n_movies, 20, input_length=1, name='item_embed')(item_input)
+    rating = layers.Dot(axes=-1)([user_x, item_x])
+    rating = layers.Flatten()(rating)
+    return models.Model([user_input, item_input], rating)
+
+
 def main(train: bool):
     args = parse_args()
 
     train_data = read_records(args.train_path)
+    test_data = read_records(args.test_path)
 
-    user_input = layers.Input(shape=(1,))
-    user_x = layers.Embedding(train_data['user_id'].max() + 1, 20, input_length=1, name='user_embed')(user_input)
-    item_input = layers.Input(shape=(1,))
-    item_x = layers.Embedding(train_data['item_id'].max() + 1, 20, input_length=1, name='item_embed')(item_input)
-    rating = layers.Dot(axes=-1)([user_x, item_x])
-    rating = layers.Flatten()(rating)
-    # rating = activations.sigmoid(rating)
-    # rating = layers.Lambda(lambda x : x * 4 + 1)(rating)
-    model = models.Model([user_input, item_input], rating)
-
-    model.summary()
+    model = cf_model(train_data['user_id'].max() + 1, train_data['item_id'].max() + 1)
     model.compile(optimizer=optimizers.Adam(1e-3),
                   loss=losses.mean_squared_error,
                   metrics=[metrics.RootMeanSquaredError()])
 
-    val_data = read_records(args.test_path)
-
     if train:
         model.fit([train_data['user_id'], train_data['item_id']], train_data['rating'],
-                  validation_data=([val_data['user_id'], val_data['item_id']], val_data['rating']),
-                  callbacks=callbacks.EarlyStopping(patience=3),
-                  batch_size=32, epochs=100)
+                  validation_data=([test_data['user_id'], test_data['item_id']], test_data['rating']),
+                  callbacks=callbacks.EarlyStopping(patience=2),
+                  batch_size=64, epochs=500, verbose=1)
         model.save_weights('checkpoints/' + path.splitext(path.basename(args.train_path))[0])
 
-    rating = layers.Lambda(lambda x : K.round(x))(rating)
+    rating = layers.Lambda(lambda x : K.minimum(K.maximum(x, 1), 5))(model.output)
     final_model = models.Model(model.input, rating)
-    final_model.compile()
+    final_model.compile(loss=losses.mean_squared_error, metrics=[metrics.RootMeanSquaredError()])
     final_model.load_weights('checkpoints/' + path.splitext(path.basename(args.test_path))[0])
-    print(final_model.evaluate([val_data['user_id'], val_data['item_id']], val_data['rating']))
+
+    final_model.evaluate([test_data['user_id'], test_data['item_id']], test_data['rating'])
+    test_data['rating'] = final_model.predict([test_data['user_id'], test_data['item_id']])
+    write_records('results/' + path.splitext(path.basename(args.test_path))[0] + '.base_prediction.txt', test_data)
 
 
 if __name__ == '__main__':
     main(True)
+    # main(False)
